c450fcf659142e06b5c456028eb4ffa6920fc5b6
Tim Hütz <tim+github@janke.biz>
2021-01-25 18:00:36 +0100

    wip: add an example for OpenAPI with rocket-rs

--- flyg-backend/src/main.rs
+++ flyg-backend/src/main.rs
@@ -1,3 +1,33 @@
+#![feature(decl_macro, proc_macro_hygiene)]
+
+use rocket::get;
+use rocket_contrib::json::Json;
+use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
+use rocket_okapi::{openapi, routes_with_openapi, JsonSchema};
+use serde::Serialize;
+
+#[derive(Serialize, JsonSchema)]
+struct Response {
+    reply: String,
+}
+
+#[openapi]
+#[get("/")]
+fn my_controller() -> Json<Response> {
+    Json(Response {
+        reply: "show me the docs!".to_string(),
+    })
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/my_resource", routes_with_openapi![my_controller])
+        .mount(
+            "/swagger",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "/my_resource/openapi.json".to_string(),
+                ..Default::default()
+            }),
+        )
+        .launch();
 }
