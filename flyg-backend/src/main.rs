d253bc64a84abf60145c912f99e61cd61708c424
Tim Hütz <tim+github@janke.biz>
2021-02-04 07:41:39 +0100

    chore: add logging support for the backend system

--- flyg-backend/src/main.rs
+++ flyg-backend/src/main.rs
@@ -1,8 +1,27 @@
+use chrono::Utc;
 use dotenv::dotenv;
+use log::LevelFilter;
 use rocket::routes;
 use std::env;
 
+fn setup_logger() {
+    let _ = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                Utc::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(LevelFilter::Trace)
+        .chain(std::io::stdout())
+        .apply();
+}
+
 fn main() {
+    setup_logger();
     dotenv().ok();
 
     // if we could get the required database url, launch rocket for handling the requests to the backend
