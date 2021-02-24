ef58045c2b5afb9d2d271efe6022334806ceaf93
Tim Hütz <tim+github@janke.biz>
2021-02-24 09:33:52 +0100

    chore: change the default docker-tag to 'latest'

--- flyg-backend/create_backend_container.sh
+++ flyg-backend/create_backend_container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-VERSION=$(cat Cargo.toml | head -n5 | grep version | awk '{ print $3 }' | sed 's/"//g')
+#VERSION=$(cat Cargo.toml | head -n5 | grep version | awk '{ print $3 }' | sed 's/"//g')
+VERSION=latest
 docker build -t flying7eleven/flyg_backend:$VERSION .
