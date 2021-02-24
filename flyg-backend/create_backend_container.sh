92c6a8ccf835dc529d2200a81ea7875d9729d036
Tim Hütz <tim+github@janke.biz>
2021-02-24 08:28:26 +0100

    chore: change the tag of the docker image

--- flyg-backend/create_backend_container.sh
+++ flyg-backend/create_backend_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 VERSION=$(cat Cargo.toml | head -n5 | grep version | awk '{ print $3 }' | sed 's/"//g')
-docker build -t flyg_backend:$VERSION .
+docker build -t flying7eleven/flyg_backend:$VERSION .
