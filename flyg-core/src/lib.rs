dc2f6f3823fe3d6d97cf45f9f4896bea7191fc31
Tim Hütz <tim+github@janke.biz>
2021-03-05 12:55:15 +0100

    docs: start documenting the flyg-core package

--- flyg-core/src/lib.rs
+++ flyg-core/src/lib.rs
@@ -4,9 +4,19 @@ pub enum Notification {
     PositionUpdate(AircraftPosition),
 }
 
+/// Store the position of the aircraft at the time of the recording.
 #[derive(Copy, Clone)]
 pub struct AircraftPosition {
+    /// The current latitude of the aircraft.
     pub latitude: f64,
+    /// The current longitude of the aircraft.
     pub longitude: f64,
+    /// The current altitude (above ground) of the aircraft.
     pub altitude: f64,
+}
+
+/// A single recording (for a specific time point) of the aircraft information.
+pub struct AircraftInformationRecord {
+    /// The current position of the aircraft.
+    pub position: AircraftPosition,
 }
